46276fe264bf9769a9bd88db4d055e4ad6732420
Thiago Garcia <thiagogds14@gmail.com>
2013-05-06 19:23:55 -0300

    Recoloca GA.

--- client/client.js
+++ client/client.js
@@ -4,8 +4,8 @@ Meteor.subscribe('Attendings');
 Meteor.subscribe('Events');
 Meteor.subscribe('FacebookEvents');
 
-//var AppTracker = new Tracker();
-//AppTracker.register(GoogleAnalyticsTracker, {account: 'UA-39154996-1'});
+var AppTracker = new Tracker();
+AppTracker.register(GoogleAnalyticsTracker, {account: 'UA-39154996-1'});
 
 var eventIDFromUrl = History.getHashByState();
 eventIDFromUrl = eventIDFromUrl.replace(/\//g, "");
