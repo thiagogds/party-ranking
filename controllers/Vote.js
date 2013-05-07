60c5f80898252087deb4d3d46873695a1e08b90a
Bruno Vargas <bruno@zephirot.(none)>
2013-05-07 15:02:58 -0300

    acertando ignore

--- controllers/Vote.js
+++ controllers/Vote.js
@@ -37,7 +37,7 @@ module.exports = function (app) {
                 if (error) {
                     response.send({error : error});
                 } else {
-                    response.send({vote});
+                    response.send({vote : vote});
                 }
             });
         });
@@ -67,7 +67,7 @@ module.exports = function (app) {
                 } else {
                     vote.remove(function (error) {
                         if (error) {
-                            response.send({error});
+                            response.send({error : error});
                         } else {
                             response.send(null);
                         }
